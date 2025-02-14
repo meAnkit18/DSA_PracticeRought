88104f534a782e90f28072275845a3b1fcbfd03d
Ankit Kumar <ankit1842kumar@gmail.com>
2025-02-14 21:45:39 +0530

    quick short

--- src/Shroting/quickshot.java
+++ src/Shroting/quickshot.java
@@ -1,4 +1,37 @@
 package Shroting;
 
+import java.util.Arrays;
+
 public class quickshot {
+    public static void main(String[] args) {
+        int arr[] = {4,6,36,2,8,4,77};
+        quickshot(arr,0, arr.length-1);
+        System.out.println("main print "+ Arrays.toString(arr));
+    }
+    static void quickshot (int arr[], int s, int e){
+        if(s<e){
+            int p = pivoit(arr,s,e);
+            quickshot(arr,0,p-1);
+            quickshot(arr,p+1,e);
+        }
+
+    }
+
+    static int pivoit(int arr[], int s, int e){
+        int j = 0;
+        int i = j-1;
+        int p = arr[e];
+        while (j<=e){
+            if (arr[j]<=p){
+                i++;
+                int t = arr[i];
+                arr[i] = arr[j];
+                arr[j] = t;
+                j++;
+            }else {
+                j++;
+            }
+        }
+        return i;
+    }
 }
