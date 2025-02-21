3b1bbd5a86eed77e34c34eabc3c2cac4a511bfca
Ankit Kumar <ankit1842kumar@gmail.com>
2025-02-21 21:41:28 +0530

    Btree

---
+++ src/treebyAC/BTree.java
@@ -0,0 +1,16 @@
+package treebyAC;
+
+public class BTree {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int val){
+            this.data = val;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
